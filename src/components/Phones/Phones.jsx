bd47e00c2e1d7c9dbd622f2a2c0aff67dd271123
shakibnymr <shakibnymr11@gmail.com>
2023-09-19 12:15:23 +0600

    bar chart worked by solving bracket issues

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,31 +1,34 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BarChart, Bar  } from 'recharts';
+import { BarChart, Bar ,XAxis, YAxis, Tooltip } from "recharts";
 
 const Phones = () => {
-    const [phones,setPhones] = useState({})
-    useEffect(()=>{
-      axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
-        .then(data => {
-            const realData = data.data.data
-            const fakeData = realData.map(phone => {
-                const obj = {
-                    name: phone.phone_name,
-    price: parseInt(phone.slug.split('-')[1])
-                }
-              return obj;
-
-              
-            }) 
-            setPhones(fakeData)
-    },[])
-    return (
-        <div>
-              <BarChart width={650} height={400} data={phones}>
-          <Bar dataKey="price" fill="#8884d8" />
-        </BarChart>
-        </div>
-    );
-})
-}
-export default Phones
+  const [phones, setPhones] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://openapi.programming-hero.com/api/phones?search=iphone")
+      .then((data) => {
+        const realData = data.data.data;
+        const fakeData = realData.map((phone) => {
+          const obj = {
+            name: phone.phone_name,
+            price: parseInt(phone.slug.split("-")[1]),
+          };
+          return obj;
+        });
+        console.log(fakeData);
+        setPhones(fakeData);
+      });
+  }, []);
+  return (
+    <div>
+      <BarChart width={850} height={400} data={phones}>
+        <Bar dataKey={"price"} fill="#8884d8" />
+      <XAxis dataKey={'name'}></XAxis>
+      <YAxis></YAxis>
+<Tooltip></Tooltip>
+      </BarChart>
+    </div>
+  );
+};
+export default Phones;
